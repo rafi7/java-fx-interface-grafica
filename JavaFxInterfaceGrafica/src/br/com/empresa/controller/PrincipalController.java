cb66c399ec422dde7ea6c0f631c13a8e57745484
jorge.alosilla@gmail.com <jorge.alosilla@gmail.com@9299010c-0cfc-4dcb-1a69-3ace980b5050>
2014-12-09 01:49:19 +0000

    Adicionado o menuBar na tela principal.fxml corrigido o caminho das imagens

--- JavaFxInterfaceGrafica/src/br/com/empresa/controller/PrincipalController.java
+++ JavaFxInterfaceGrafica/src/br/com/empresa/controller/PrincipalController.java
@@ -8,53 +8,89 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import br.com.empresa.app.Principal;
 
-public class PrincipalController{
-	
-	 @FXML
-	    private Label lbMensagem;
+public class PrincipalController {
 
-	    @FXML
-	    private Button btCadCliente;
+	@FXML
+	private Button btCadProduto;
 
-	    @FXML
-	    private Button btCadFornecedor;
+	@FXML
+	private MenuItem miClientes;
 
-	    @FXML
-	    private Label desenvolvidoPor;
+	@FXML
+	private Button btCadCliente;
 
-	    @FXML
-	    void cadastrarFornecedor(ActionEvent event){
-	    	Parent root;
-			try {
-				root = FXMLLoader.load(getClass().getResource("/br/com/empresa/view/fornecedor.fxml"));
-				Principal.SCENE.setRoot(root);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-	    }
+	@FXML
+	private MenuItem miSobre;
 
-	    @FXML
-	    void cadastrarCliente(ActionEvent event){
-	    	Parent root;
-			try {
-				root = FXMLLoader.load(getClass().getResource("/br/com/empresa/view/cliente.fxml"));
-				Principal.SCENE.setRoot(root);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-	    }
-	    
-	    @FXML
-	    void cadastrarProduto(ActionEvent event){
-	    	Parent root;
-			try {
-				root = FXMLLoader.load(getClass().getResource("/br/com/empresa/view/produto.fxml"));
-				Principal.SCENE.setRoot(root);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-	    }
-	    
+	@FXML
+	private Button btCadFornecedor;
+
+	@FXML
+	private Menu mnAjuda;
+
+	@FXML
+	private MenuItem miProdutos;
+
+	@FXML
+	private Label desenvolvidoPor;
+
+	@FXML
+	private MenuBar menuBar;
+
+	@FXML
+	private Menu mnCadastros;
+
+	@FXML
+	private Label lbMensagem;
+
+	@FXML
+	private MenuItem miSair;
+
+	@FXML
+	private MenuItem miFornecedores;
+
+	@FXML
+	private Menu mnArquivo;
+
+	@FXML
+	void cadastrarFornecedor(ActionEvent event) {
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource(
+					"/br/com/empresa/view/fornecedor.fxml"));
+			Principal.SCENE.setRoot(root);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	void cadastrarCliente(ActionEvent event) {
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource(
+					"/br/com/empresa/view/cliente.fxml"));
+			Principal.SCENE.setRoot(root);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	void cadastrarProduto(ActionEvent event) {
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource(
+					"/br/com/empresa/view/produto.fxml"));
+			Principal.SCENE.setRoot(root);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
